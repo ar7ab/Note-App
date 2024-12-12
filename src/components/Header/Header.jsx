145b2eeb1b41a95586909a421f0f2c949a46ed0c
Ahmed Arhab <ahmedarhab018@gmail.com>
2024-12-12 04:18:26 +0400

    finish layout & responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom";
-import Logo from "./Logo";
+import Logo from "./Logo"; // Ensure you have a Logo component
 import SearchBox from "./SearchBox";
 import { IoAddSharp } from "react-icons/io5";
 
 const Header = () => {
   return (
-    <div className="px-3 overflow-hidden sm:px-24 bg-[#ffffff] w-full hidden shadow-lg sm:py-4 py-4  items-center gap-1.5 sm:gap-5">
+    <div className="px-3 flex sm:px-24 bg-[#ffffff] w-full shadow-lg sm:py-4 py-4 items-center gap-1.5 sm:gap-5 flex-wrap">
       {/* Logo */}
       <Logo />
       {/* Search Box */}
@@ -14,8 +14,8 @@ const Header = () => {
       <div className="add-btn">
         <NavLink
           to={"/addnote"}
-          className="flex justify-between items-center gap-0.5 sm:gap-1 bg-[#3FA2F6] 
-          text-white outline-none border-none  py-1.5 sm:py-2 px-2 sm:px-4 rounded-full 
+          className="flex justify-between items-center w-fit gap-0.5 sm:gap-1 bg-[#3FA2F6] 
+          text-white outline-none border-none py-1.5 sm:py-2 px-2 sm:px-4 rounded-full 
           hover:bg-[#0D92F4] transition-all duration-300 ease-linear"
         >
           <IoAddSharp className="text-xl" />
