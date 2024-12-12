1f966c974ee53559e841bd38ba5e812e8b245463
Ahmed Arhab <ahmedarhab018@gmail.com>
2024-12-12 20:17:07 +0400

    Implement add and delete operations with context

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { HashRouter as Router } from "react-router-dom";
-import { NotesProvider } from "./context/notesContext.jsx";
+import { NotesProvider } from "./context/NotesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
